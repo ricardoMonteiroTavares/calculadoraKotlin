0edbd22b7eaabfed43dc7b9762ae78b85ae30706
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-21 23:16:55 -0300

    Criação da função de potência

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,3 +12,13 @@ fun multiplication(m: Float, n: Float):Float = m.times(n)
 fun division(m: Float, n: Float):Float = m.div(n)
 
 fun mod(m: Float, n: Float):Float = m.rem(n)
+
+fun power(m: Float, n: Float):Float {
+    if (n == 0f) return 1f
+
+    var base:Float = m
+    val inverter:Float = -1f
+
+    if (m < 0) return multiplication(multiplication(base, inverter).pow(n), inverter)
+    return m.pow(n)
+}
